10f7bed62e35c98b500edaa2cbdc591531aa1687
Gabriel Bursi <gabriellbursii@gmail.com>
2025-05-30 23:19:26 -0300

    chore: backend structure

---
+++ backend/agileflow/src/test/java/com/agileflow/agileflow/AgileflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agileflow.agileflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
